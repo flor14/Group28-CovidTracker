654e839f0ab85385028b4a93896ea6ea4b042db4
jessie14 <jessiewong07@gmail.com>
2022-01-13 19:42:31 -0800

    Change function input from csv to dataframe

--- src/CovidTracker/plot_geographical.py
+++ src/CovidTracker/plot_geographical.py
@@ -1,13 +1,13 @@
-def plot_geographical(input_file):
+def plot_geographical(df):
     """Creates a chloropleth map showing the number 
        of covid cases in each province of Canada
 
     Parameters
     ----------
-    input_file : str
-        Path to data file.
+    df : pandas.DataFrame
+        Pandas dataframe containing covid data to plot.
 
     Examples
     --------
-    >>> plot_geographical("data.csv")
+    >>> plot_geographical(covid_df)
     """
