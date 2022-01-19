d8a95b81ea8ae3c4eb8d1cc810d9e822ad9a983e
Cuthbert Chow <cuthbert.chow@gmail.com>
2022-01-18 16:14:15 -0800

    Update Test Documentation For get_covid_data()

--- tests/test_getcoviddata.py
+++ tests/test_getcoviddata.py
@@ -4,11 +4,22 @@ import pytest
 
 
 def test_get_covid_data():
+    """Test the get_covid_data() function"""
+
+    # Tests that the function returns the correct data for given arguments
+    test_df = pd.DataFrame({'cases': [698], 'cumulative_cases': [161969], 'date_report': ['25-08-2021'], 'province': ['BC']})
+    assert get_covid_data('cases', 'BC', date = '25-08-2021').equals(test_df)
+
+    # Tests that the function returns a DataFrame of correct size 
     assert isinstance(get_covid_data(), pd.DataFrame)
-    assert len(get_covid_data('cases', date = '22-11-2021')) == 14
+    assert get_covid_data('cases', date = '22-11-2021').shape == (14, 4)
 
 
 def test_get_covid_data_errors():
+    """Test that get_covid_data() raises the correct errors"""
+
+    # Tests that ValueErrors are raised when arguments are of right type but inappropriate value
     with pytest.raises(ValueError):
         get_covid_data(date = '33-22-1997')
         get_covid_data('deaths')
+        get_covid_data(loc = "HK")
