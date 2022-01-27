0489bb1cda711756054abee1a459ee21e65f9a9b
Tianwei Wang <wangtianwei@hotmail.com>
2022-01-27 15:07:42 -0800

    Modify the output dataframe columns name

--- src/CovidTracker/calculate_stat_summary.py
+++ src/CovidTracker/calculate_stat_summary.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 def calculate_stat_summary(df, column):
     """Creates summary information about the 
-       covid information in each province of Canada
+       covid cases in each province of Canada
 
     Parameters
     ----------
@@ -23,6 +23,7 @@ def calculate_stat_summary(df, column):
     >>> calculate_stat_summary(covid_df, 'cumulative_deaths')
     """
 
+
     # Check input dataframe validity
     if not isinstance(df, pd.DataFrame):
         raise TypeError("Invalid argument type: df must be a pandas DataFrame")
@@ -31,8 +32,8 @@ def calculate_stat_summary(df, column):
     elif not ('province' in df.columns):
         raise ValueError("Argument value error: df must contain province columns")
 
-    # Find the date column
     columns = list(df.columns)
+
     for i in range(len(columns)):
         if columns[i].startswith('date'):
             date_col = columns[i]
@@ -40,20 +41,21 @@ def calculate_stat_summary(df, column):
 
     # Check column name validity
     if not isinstance(column, str):
-        raise TypeError("Invalid argument type: column name must be a string")
+        raise TypeError("Invalid argument type: column must be a string")
     elif column not in columns:
         raise ValueError("The column name does not exist in the dataframe,\
-                        choose a valid column name")
+                        Choose a valid column name")
 
     # Check the data type of the column
     if not (df[column].dtype == 'int64' or df[column].dtype == 'float64'):
         raise TypeError("Invalid argument type: this column must be numeric")
 
-    # Select the up-to-date information of each province
+    # Select the up to date information of each province
     df[date_col] = pd.to_datetime(df[date_col], format='%d-%m-%Y')
-    max_date = df.loc[df[date_col].argmax(), date_col]
-    columns = [date_col, 'province'] + list(set(columns) - set([date_col, 'province']))
-    summary = df[df[date_col] == max_date][columns].sort_values('province')
+    start_date = df.loc[df[date_col].argmin(), date_col]
+    end_date = df.loc[df[date_col].argmax(), date_col]
+    columns = [date_col, 'province'] + [column]
+    summary = df[df[date_col] == end_date][columns].sort_values('province')
 
     # Summarize the min, max and mean of the selected summary column
     min_value = []
@@ -77,13 +79,14 @@ def calculate_stat_summary(df, column):
         std.append(int(summ[2]))
         count.append(int(summ[0]))
 
-    summary[column + '_min'] = min_value
-    summary[column + '_max'] = max_value
-    summary[column + '_mean'] = mean_value
-    summary[column + '_25%'] = percentile_25
-    summary[column + '_50%'] = percentile_50
-    summary[column + '_75%'] = percentile_75
-    summary[column + '_std'] = std
-    summary[column + '_count'] = count
+    summary['min'] = min_value
+    summary['max'] = max_value
+    summary['mean'] = mean_value
+    summary['25%'] = percentile_25
+    summary['50%'] = percentile_50
+    summary['75%'] = percentile_75
+    summary['std'] = std
+    summary['count'] = count
+    summary['start_date'] = start_date
     
-    return(pd.DataFrame(summary))
+    return(summary)
